0b68cf8bfe845edd6900d263ee7909bc5a4f0185
VPontis15 <94546957+VPontis15@users.noreply.github.com>
2023-10-14 23:55:44 +0300

    finish static phase

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import "./index";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Bill />
+      <ServiceRating />
+      <FriendsServiceRating />
+      <Total />
     </div>
   );
 }
 
-export default App;
+function Bill() {
+  return (
+    <div className="row">
+      <label for="bill">How much was the bill?</label>
+      <input type="text" id="bill"></input>
+    </div>
+  );
+}
+
+function ServiceRating() {
+  return (
+    <div className="row">
+      <label for="service">How did you like the service</label>
+      <select id="service">
+        <option value={0.1}> It was good (10%)</option>
+        <option value={0.15}> It was great (15%)</option>
+        <option value={0.2}> Absolutely amazing (20%)</option>
+      </select>
+    </div>
+  );
+}
+
+function FriendsServiceRating() {
+  return (
+    <div className="row">
+      <label for="friend">How did your friend the service</label>
+      <select id="friend">
+        <option value={0.1}> It was good (10%)</option>
+        <option value={0.15}> It was great (15%)</option>
+        <option value={0.2}> Absolutely amazing (20%)</option>
+      </select>
+    </div>
+  );
+}
+
+function Total() {
+  return (
+    <div className="row">
+      <h1>You pay $X ($X + $X tip)</h1>
+    </div>
+  );
+}
